c37b43fdb6c9dc94f809b26b8dee0e0e9167b024
Paulo Cesar Cordeiro Lima <paulo.lima@sindiabc.local>
2025-11-19 15:49:53 -0300

    Saving code

--- db/db_test_node.sql
+++ db/db_test_node.sql
@@ -8,4 +8,15 @@ email varchar(100),
 primary key(id)
 );
 
+create table adminApp(
+id_admin int auto_increment,
+userName varchar(50),
+userPassword varchar(50),
+PRIMARY KEY(id_admin)
+);
+
+INSERT INTO adminApp (userName, userPassword)
+VALUES ('paulo', '123456');
+
 SELECT * FROM infoUsers;
+SELECT * FROM adminApp;
